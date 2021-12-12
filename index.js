bbd7a2f4e36b127b5adb715140a5051e5b63f43b
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2021-12-12 23:30:48 +0800

    ff

---
+++ index.js
@@ -0,0 +1,9 @@
+let hide = document.getElementById('hide');
+let check = document.getElementsByClassName('check');
+
+
+function hideMenu() {
+    console.log('hello');
+    check.classList.toggle('show');
+
+}
