2240e167cd8487aefea4410e16782ad71a51cbce
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2021-12-14 00:19:36 +0800

    jj

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let hide = document.getElementById('hide');
 let navbar = document.getElementById("navbar");
 let link = document.querySelectorAll('link')
 
-console.log(link)
 function hideMenu() {
-    console.log('hello');
     navbar.classList.add('show');
 }
