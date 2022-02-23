d40635093c3976aeab6bdc618c46eaba7af0f874
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-02-24 01:50:15 +0800

    server

--- server.js
+++ server.js
@@ -1,29 +1,67 @@
 const http = require('http');
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     const { headers, url, method } = req;
     res.setHeader("content-type", "text/html");
     if (url === '/') {
-        res.statusCode = 247;
-        res.write("<h1>Welcome to my first back-end project</h1>")
-        res.write(`<br><br> Login hiih <a href="/login">dar</a>`);
-        res.end();
+        fs.readFile('./train/index.html', 'utf-8', (error, data) => {
+            if (error) {
+                res.statusCode = 500;
+                res.write('<h1>Error!</h1>');
+                res.end();
+            } else {
+                res.statusCode = 200;
+                res.write(data);
+                res.end();
+            }
+        });
     } else if (url === '/login') {
-        res.statusCode = 247;
-        res.write("<h1>login hiih</h1>")
-        res.write(`<form action="/logincheck" method="POST">`);
-        res.write(`<br><input type="text" name="email"/>`);
-        res.write(`<br><input type="password" name="password"/>`);
-        res.write(`<br><input type="submit" value="login"/>`);
-        res.write(`<form/>`);
-        res.end();
+        fs.readFile('./train/login.html', 'utf-8', (error, data) => {
+            if (error) {
+                res.statusCode = 500;
+                res.write('<h1>Error!</h1>');
+                res.end();
+            } else {
+                res.statusCode = 200;
+                res.write(data);
+                res.end();
+            }
+        });
     } else if (url === '/logincheck' && method === "POST") {
-        res.statusCode = 247;
-        res.write(`<h1>Login hiiw</h1>`);
-        res.write("<h1>" + method + "</h1>");
-        res.end();
+        const body = [];
+        req.on("data", chunk => {
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const pass = parsedBody.split('=')[2];
+            // res.write("huleej awlaa")
+            // res.end();
+            if (pass === "muumaa1") {
+                res.statusCode = 302;
+                res.setHeader("Location", "/home");
+                res.end();
+            } else {
+                res.statusCode = 302;
+                res.setHeader("Location", "/error");
+                res.end();
+            }
+        });
     } else if (url === '/home') {
-        //login daraa
+        fs.readFile('./train/home.html', 'utf-8', (error, data) => {
+            res.statusCode = 200;
+            res.write(data);
+            res.end();
+
+        });
+    } else if (url === '/error') {
+        fs.readFile('./train/error.html', 'utf-8', (error, data) => {
+            res.statusCode = 200;
+            res.write(data);
+            res.end();
+
+        });
     } else {
         res.statusCode = 404;
         res.write(`<h1>404</h1>`);
