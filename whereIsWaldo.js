412a2c318d6f5f5a70c32db1b1119532fb3941a6
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-01 00:55:12 +0800

    dhlah

--- whereIsWaldo.js
+++ whereIsWaldo.js
@@ -1,11 +1,10 @@
-
 const arrd = [
-  ["O", "O", "O", "P"],
-  ["O", "O", "O", "O"],
-  ["O", "O", "O", "O"],
-  ["O", "O", "O", "O"],
-  ["O", "O", "O", "O"],
-  ["O", "O", "O", "O"],
+    ["O", "O", "O", "P"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
 ];
 const ar1 = [
     ["A", "A", "A"],
@@ -16,22 +15,22 @@ let ll = "";
 let r;
 let c;
 const whereIsWaldo = (arr) => {
-  if (arr[0][0] === arr[0][1]) {
-    ll = arr[0][0];
-  }else if(arr[0][0] === arr[0][2] && arr[0][0] !== arr[0][1]){
-    ll = arr[0][0];
-  }else{
-    ll = arr[0][1];
-  }
-  arr.forEach((cur, index, element, row) => {
-    cur.forEach((el, i) => {
-      if (el !== ll) {
-          r = index + 1;
-          c = i + 1;
-        }
+    if (arr[0][0] === arr[0][1]) {
+        ll = arr[0][0];
+    } else if (arr[0][0] === arr[0][2] && arr[0][0] !== arr[0][1]) {
+        ll = arr[0][0];
+    } else {
+        ll = arr[0][1];
+    }
+    arr.forEach((cur, index, element, row) => {
+        cur.forEach((el, i) => {
+            if (el !== ll) {
+                r = index + 1;
+                c = i + 1;
+            }
+        });
     });
-});
-console.log(r , c);
+    console.log(r, c);
 };
 whereIsWaldo(ar1);
 
@@ -68,4 +67,43 @@ const users = [
     { id: 47, name: 'Bold', age: 28, group: 'admin' },
     { id: 85, name: 'Tulga', age: 34, group: 'editor' },
     { id: 97, name: 'Suren', age: 28, group: 'admin' }
-];  
+];
+const pure = (arr, str) => {
+    return arr.filter(officer => officer.group <= str);
+}
+console.log(pure(users, 'admin'));
+
+const array1 = [1, 4, 9, 16];
+const aaa = [];
+const myMap = (arr) => {
+        arr.forEach((cur, index, element) => {
+            aaa.push(cur * 2);
+        })
+        console.log(aaa);
+    }
+    //const map1 = array1.map(x => x * 2);
+myMap(array1)
+
+const users1 = [
+    { id: 11, name: 'bat', age: 23, group: 'editor' },
+    { id: 47, name: 'bold', age: 28, group: 'admin' },
+    { id: 85, name: 'bulga', age: 34, group: 'editor' },
+    { id: 97, name: 'buren', age: 28, group: 'admin' }
+];
+// let b = [];
+// const up = (str) => {
+//     b = str.map(x => x.name);
+//     b.toUpperCase();
+//     console.log(b);
+// }
+// up(users1)
+let numbers = [3, -1, 8, 0, 2];
+let re;
+const sum1 = (acc) => {
+    acc.reduce((previousValue, currentValue) => {
+        re = previousValue + parseInt(currentValue), 0;
+        return re;
+    })
+    console.log(re);
+}
+sum1(numbers);
