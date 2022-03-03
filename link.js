016c153caa6ebd4445b8343e7f95e85ed1f9383b
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-04 00:08:54 +0800

    ndladn

--- link.js
+++ link.js
@@ -1,49 +1,17 @@
 import axios from "axios";
-let arr;
-let link;
-let maxRead = 0;
-let $index = -1;
-const req = async (url, index) => {
-    let res = await axios.get(url);
+const req = async() => {
+    const res = await axios.get('https://www.poemist.com/api/v1/randompoems')
+    const promises = res.data.map(async(po) => {
+        const res = await axios.get(po.url);
+        const totalRead = parseInt(res.data.split('Total read')[0].split('</i>').pop());
+        return {
+            title: po.title,
+            totalRead: totalRead,
+        }
+    });
+    const po = await Promise.all(promises);
+    console.log(po);
+    const top = po.sort((p1, p2) => p2.totalRead - p1.totalRead)[0];
+    console.log(top);
 }
-axios.get('https://www.poemist.com/api/v1/randompoems').then(resp => {
-    arr = resp.data;
-    link = arr.map(officer => officer.url);
-    console.log(link);
-    axios.get(link[0], link[1], link[2], link[3], link[4]).then(res => {
-        console.log(res.data);
-    })
-});
-
-// const htmlReguest = async(url, index) => {
-
-//     let response = await axios.get(url);
-//     const helperArr = response.data.split('\n').join('').split(' ');
-//     let readNumber = helperArr[helperArr.indexOf('Total') - 1];
-//     if (maxRead < Number(readNumber)) {
-//         maxRead = readNumber;
-//         $index = index
-//         return $index
-//     }
-//     return $index;
-
-// }
-// const problem = async() => {
-//     let response = await axios.get('https://www.poemist.com/api/v1/randompoems%27)
-//     let url = [];
-//     const savePoemist = [];
-
-//     response.data.forEach(element => {
-//         savePoemist.push(element['content']);
-//         url.push(element['url']);
-//     });
-//     let pro = htmlReguest(url[0], 0);
-//     let pro1 = htmlReguest(url[1], 1);
-//     let pro2 = htmlReguest(url[2], 2);
-//     let pro3 = htmlReguest(url[3], 3);
-//     let pro4 = htmlReguest(url[4], 4);
-//     Promise.all([pro, pro1, pro2, pro3, pro4]).then((resultArr) => {
-//         console.log(most total read: ${maxRead}\n${savePoemist[resultArr[4]]});
-//     })
-// }
-// problem();
+req();
---
+++ weather.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const req = async(loca) => {
+    const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${loca.toString()}.json?access_token=pk.eyJ1Ijoic2Fpa2hhbmJpbGVnIiwiYSI6ImNsMGFyaGs2NTAxM20za3A0cWFxMzd5YWMifQ.Gabbw102QC01jkqp0R1VmQ`);
+    const arr = []
+    arr.push(res.data)
+        //console.log(res.data);
+    const promises = arr.map(async(po) => {
+        return {
+            text: po.text,
+            center: po.center,
+            name: po.place_name,
+        }
+    });
+    const po = await Promise.all(promises);
+    console.log(po);
+}
+req('ulaanbaatar')
+    //const res = await axios.get(po.center);
+    //const totalRead = parseInt(res.data.split('Total read')[0].split('</i>').pop());
