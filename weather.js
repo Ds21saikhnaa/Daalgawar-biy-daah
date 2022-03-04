bf95537f1566c8796cc98b7ad31a397800abfb7a
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-04 16:21:13 +0800

    trash

--- weather.js
+++ weather.js
@@ -3,16 +3,17 @@ const req = async(loca) => {
     const res = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${loca.toString()}.json?access_token=pk.eyJ1Ijoic2Fpa2hhbmJpbGVnIiwiYSI6ImNsMGFyaGs2NTAxM20za3A0cWFxMzd5YWMifQ.Gabbw102QC01jkqp0R1VmQ`);
     const arr = []
     arr.push(res.data)
-        //console.log(res.data);
+    console.log(arr.length);
     const promises = arr.map(async(po) => {
+        console.log(po.length);
         return {
             text: po.text,
             center: po.center,
             name: po.place_name,
         }
     });
-    const po = await Promise.all(promises);
-    console.log(po);
+    //const po = await Promise.all(promises);
+    //console.log(po);
 }
 req('ulaanbaatar')
     //const res = await axios.get(po.center);
