2d2b9648626aea1a802db73fb35eed9ab4961e1b
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-07 14:54:32 +0800

    das

---
+++ firstreact/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Table, But } from "./Reac";
+const myArray = [<Table />]
+function App() {
+  return (
+    <div className="App">
+      <Table />
+      <But />
+      {/* <h1>snake</h1>
+      {myArray.map((value, index) => (  
+        value
+     ))}   */}
+    </div>
+  );
+}
+
+export default App;
---
+++ firstreact/src/Reac.js
@@ -0,0 +1,14 @@
+import './reac.css';
+export const Table = (props) => {
+    return (
+        <h1>You clicked { props.clicked } times</h1>
+    )
+}
+export const But = (props) => {
+    return (
+        <button >click me</button>
+    )
+}
+// { for(let i = 0; i < 10; i ++) {
+
+// }}
