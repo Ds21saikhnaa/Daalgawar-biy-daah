2fdac4c81af77fc9df5114b0b7170b4ae4da57ea
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-09 12:55:26 +0800

    dah

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,18 +1,38 @@
 import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 import { Click } from './cli.js'
 import { Board } from './table.js'
+import { Battle } from './battle.js'
 
 function App() {
+  const [count, setCount] = useState(50)
+  const [countRed, setCountRed] = useState(50)
+  const divClick = () => {
+    setCount(count + 1);
+}
+const divClicked = () => {
+  setCountRed(countRed + 1);
+}
   return (
     <div className='Container'>
-      <div className='One'>
-        <h1 className='Row'>You clicked { <Click />} times</h1>
+      <div className='Nav'>
+        <h1>red<span>{ count }</span>:<span>{ countRed }</span>blue</h1>
       </div>
-      <div className='Two'>
-        <Board></Board>
+      <div className='Mini'>
+        <div className='Left' onClick={ divClick } style={{ width: `${ count }%` }}></div>
+        <div className='Right' onClick={ divClicked } style={{ width: `${ countRed }%` }}></div>
       </div>
     </div>
+
+
+//   <div className='One'>
+//     <h1 className='Row'>You clicked { <Click />} times</h1>
+//   </div>
+//   <div className='Two'>
+//     <Board></Board>
+//   </div>
+
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
