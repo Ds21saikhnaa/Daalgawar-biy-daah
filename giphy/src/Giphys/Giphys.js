47d6dcfc13665d2db3536540eaaf362e772bc7e6
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-03-11 01:48:16 +0800

    done

---
+++ giphy/src/Giphys/Giphys.js
@@ -0,0 +1,14 @@
+// import React, { useState, useEffect } from 'react'
+// const[word, setWord] = useState('');
+// const handInput = (e) => {
+//     setWord(e.target.value)
+// }
+// const Inp = () => {
+//     <input
+//                 placeholder='Update your item'
+//                 value={word}
+//                 onChange={handInput}
+//                 name='text'
+//                 className='todo-input edit'
+//               />
+// } 
